977c97aadfc5f920c5f1966f661a5d6aad517644
CiroLee <ciromail@126.com>
2025-03-26 11:55:58 +0800

    style: optimize installation page

--- src/components/business/TechnicalGuide.tsx
+++ src/components/business/TechnicalGuide.tsx
@@ -16,7 +16,7 @@ export default function TechnicalGuide({ guides, className }: TechnicalGuideProp
     <div className={cn(className)}>
       {guides.map((item, index) => (
         <div id={item.id} key={item.id} className="before:bg-line relative flex gap-3 not-last:pb-8 before:absolute before:top-8 before:left-3.5 before:h-[calc(100%_-_36px)] not-last:before:w-px">
-          <span className="flex size-7 shrink-0 items-center justify-center rounded-full bg-neutral-700 text-sm">{index + 1}</span>
+          <span className="flex size-7 shrink-0 items-center justify-center rounded-full bg-neutral-200 text-sm dark:bg-neutral-700">{index + 1}</span>
           <div>
             <Show if={item.title}>
               <Heading as="h5" className="mb-4">
