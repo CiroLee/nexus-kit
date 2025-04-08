b8c46effcba9b26e91fec50781d0f32fc9d224ec
CiroLee <ciromail@126.com>
2025-04-08 22:17:25 +0800

    style: update loading style

--- src/app/examples/loading.tsx
+++ src/app/examples/loading.tsx
@@ -1,7 +1,7 @@
 import { IconLoader } from '@tabler/icons-react';
 export default function Loading() {
   return (
-    <div className="flex size-full items-center justify-center">
+    <div className="flex h-screen w-screen items-center justify-center">
       <IconLoader className="animate-spin" size={20} />
     </div>
   );
