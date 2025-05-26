2852f21ae168eedd16fbcc287863bbcdf2b33a88
CiroLee <ciromail@126.com>
2025-05-26 16:44:41 +0800

    feat: add sourcecode

--- src/app/docs/centers/page.tsx
+++ src/app/docs/centers/page.tsx
@@ -1,10 +1,11 @@
 import SectionIntro from '@/components/business/SectionIntro';
 import PreviewAndCode from '@/components/business/PreviewAndCode';
 import CodeBox from '@/components/business/CodeBox';
-// import CodeDrawer from '@/components/business/CodeDrawer';
+import CodeDrawer from '@/components/business/CodeDrawer';
 import OnThisPage, { AnchorItem } from '@/components/business/OnThisPage';
 import Center from '@/components/ui/Center';
 import { defaultCode, centerXCode, centerYCode } from '@/codes/demos/code.centers';
+import { getSourceCode } from '@/app/api/github';
 
 const navList: AnchorItem[] = [
   { anchorId: 'default', label: 'default' },
@@ -12,11 +13,13 @@ const navList: AnchorItem[] = [
   { anchorId: 'center-y', label: 'centerY' },
 ];
 
-export default function CenterPage() {
+export default async function CenterPage() {
+  const sourceCode = await getSourceCode('Center/index.tsx');
   return (
     <div className="flex">
       <div className="main-container">
         <SectionIntro title="Center" description="Center is a layout component that centers its children." />
+        <CodeDrawer code={sourceCode} />
         <PreviewAndCode anchorId="default" title="default" codeText={defaultCode} code={<CodeBox code={defaultCode} />}>
           <Center display="flex" center className="h-20 w-80 rounded bg-neutral-200 dark:bg-neutral-700">
             center
