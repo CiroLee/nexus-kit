69c11ea5dad486325f073714606cb58c5d4d813c
CiroLee <ciromail@126.com>
2025-06-26 14:08:23 +0800

    feat: update Popover component

--- src/components/ui/Popover/index.tsx
+++ src/components/ui/Popover/index.tsx
@@ -4,18 +4,18 @@ import { cn } from '@/lib/utils';
 import { cva } from 'class-variance-authority';
 
 const popoverContent = cva(
-  `rounded-md bg-background drop-shadow-[0_-1px_0,1px_0_0,0_1px_0,-1px_0_0] drop-shadow-line p-2 z-(--popup) transition-all origin-(--radix-popover-content-transform-origin) 
+  `rounded-md bg-background drop-shadow-[0_-1px_0,1px_0_0,0_1px_0,-1px_0_0] drop-shadow-line p-2 z-(--popup) transition-all origin-(--radix-popover-content-transform-origin)
   animate-zoom-fade-in data-[state=closed]:animate-zoom-fade-out outline-none`,
 );
 
 interface PopoverProps extends React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Root>, React.ComponentPropsWithRef<typeof PopoverPrimitive.Content> {
-  trigger: React.ReactNode;
+  trigger?: React.ReactNode;
   hiddenArrow?: boolean;
 }
 export function Popover({ trigger, children, hiddenArrow, className, sideOffset = 5, alignOffset = 1, defaultOpen, open, onOpenChange, modal, ...props }: PopoverProps) {
   return (
     <PopoverPrimitive.Root defaultOpen={defaultOpen} open={open} onOpenChange={onOpenChange} modal={modal}>
-      <PopoverPrimitive.Trigger asChild>{trigger}</PopoverPrimitive.Trigger>
+      {trigger && <PopoverPrimitive.Trigger asChild>{trigger}</PopoverPrimitive.Trigger>}
       <PopoverPrimitive.Portal>
         <PopoverPrimitive.Content className={cn(popoverContent({ className }))} sideOffset={sideOffset} alignOffset={alignOffset} {...props}>
           {children}
