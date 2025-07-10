846e468bafbf463853941cc2c1e9400c08380d61
CiroLee <ciromail@126.com>
2025-07-10 17:53:39 +0800

    style: update UserDemo style

--- src/components/demos/User.tsx
+++ src/components/demos/User.tsx
@@ -7,21 +7,21 @@ export default function UserDemo() {
   return (
     <Card className="p-1.5">
       <div className="relative h-60 overflow-hidden rounded-sm">
-        <div className="bg-background/80 hover:bg-background/60 group absolute top-2 right-2 z-3 flex size-8 items-center justify-center rounded-lg backdrop-blur-md backdrop-saturate-200 transition-colors">
-          <IconHeartFilled size={14} className="opacity-60 group-hover:opacity-100" />
+        <div className="bg-background/70 hover:bg-background/55 group absolute top-2 right-2 z-3 flex size-8 items-center justify-center rounded-lg backdrop-blur-md backdrop-saturate-200 transition-colors">
+          <IconHeartFilled size={14} className="text-neutral-500 opacity-60 group-hover:opacity-100 dark:text-neutral-300" />
         </div>
         <ul className="absolute bottom-4 z-3 grid w-full grid-cols-3 justify-items-center gap-2">
           <li className="flex items-end gap-0.5">
-            <p className="text-xl leading-4 font-medium">324</p>
-            <IconUser size={14} className="text-description" />
+            <p className="text-xl leading-4 font-medium text-white">324</p>
+            <IconUser size={14} className="text-neutral-300" />
           </li>
           <li className="flex items-end gap-1">
-            <p className="text-xl leading-4 font-medium">235</p>
-            <IconUsers size={14} className="text-description" />
+            <p className="text-xl leading-4 font-medium text-white">235</p>
+            <IconUsers size={14} className="text-neutral-300" />
           </li>
           <li className="flex items-end gap-1">
-            <p className="text-xl leading-4 font-medium">34</p>
-            <IconHeartFilled size={14} className="text-neutral-400" />
+            <p className="text-xl leading-4 font-medium text-white">34</p>
+            <IconHeartFilled size={14} className="text-neutral-300" />
           </li>
         </ul>
         <div className="absolute inset-0 z-2 bg-gradient-to-t from-black/70 from-20% to-white/10"></div>
