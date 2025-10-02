069e7bfa42473c88ab1dcf6fefa6eccb213916cb
CiroLee <ciromail@126.com>
2025-10-02 23:41:02 +0800

    fix: update Heading

--- src/components/ui/Heading/index.tsx
+++ src/components/ui/Heading/index.tsx
@@ -9,7 +9,7 @@ const heading = cva('font-normal', {
       h2: 'text-[2rem]/[1.4] tracking-[0.3px] font-semibold',
       h3: 'text-[1.75rem]/[1.5] tracking-[0.2px] font-semibold',
       h4: 'text-[1.5rem]/[1.5] tracking-[0.1px] font-medium',
-      h5: 'text-[1,25rem]/[1.6] tracking-0 font-medium',
+      h5: 'text-[1.25rem]/[1.6] tracking-0 font-medium',
       h6: 'text-[1rem]/[1.5] -tracking-[0.2px]',
     },
   },
