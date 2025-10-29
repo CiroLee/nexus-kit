20bb5cfa814180aad514f8b907402d69fbf56db3
CiroLee <ciromail@126.com>
2025-10-29 18:13:06 +0800

    feat: update NativeDialog source code

--- src/app/docs/native-dialogs/page.tsx
+++ src/app/docs/native-dialogs/page.tsx
@@ -1,6 +1,6 @@
 import DialogMain from './main';
 import { getSourceCode } from '@/app/api/github';
 export default async function DialogPage() {
-  const sourceCode = await getSourceCode('Dialog/index.tsx');
+  const sourceCode = await getSourceCode('NativeDialog/index.tsx');
   return <DialogMain sourceCode={sourceCode} />;
 }
