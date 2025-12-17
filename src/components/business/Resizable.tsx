a961ace5e9d2a17351866012ebf01424c84494bb
CiroLee <ciromail@126.com>
2025-12-17 13:51:42 +0800

    fix: update Resizable component

--- src/components/business/Resizable.tsx
+++ src/components/business/Resizable.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { cn } from '@/lib/utils';
 import { IconArrowBarBoth } from '@tabler/icons-react';
-import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
+import { Panel, Group, Separator } from 'react-resizable-panels';
 interface ResizeBoxProps {
   className?: string;
   children?: React.ReactNode;
@@ -9,22 +9,22 @@ interface ResizeBoxProps {
 
 function ResizeHandler() {
   return (
-    <PanelResizeHandle className="relative !flex-1">
+    <Separator className="relative flex-1!">
       <div className="transition-color top-1/2 flex h-full w-5 items-center justify-center bg-neutral-200 dark:bg-neutral-700">
         <IconArrowBarBoth size={14} className="text-neutral-500 dark:text-neutral-300" />
       </div>
-    </PanelResizeHandle>
+    </Separator>
   );
 }
 export default function Resizable(props: ResizeBoxProps) {
   const { children, className } = props;
   return (
-    <PanelGroup direction="horizontal" className={cn('relative', className)}>
-      <Panel defaultSize={100} className="h-full min-w-80">
+    <Group className={cn('relative', className)}>
+      <Panel defaultSize="100%" minSize={320} className="h-full">
         {children}
       </Panel>
       <ResizeHandler />
       <Panel defaultSize={0} />
-    </PanelGroup>
+    </Group>
   );
 }
